4ab6d1437af3481d910cb4ffa867a5128648cf36
Curio5813 <marcosglasner@gmail.com>
2025-04-25 10:18:55 +0100

    Adicioando o programa 'ImpostoDeRenda.java' ao repositório.

--- src/BeecrowdIniciante/ImpostoDeRenda.java
+++ src/BeecrowdIniciante/ImpostoDeRenda.java
@@ -21,13 +21,14 @@ public class ImpostoDeRenda {
 			System.out.println("Isento");
 		}
 		else if(2000.01 <= rendimentos && rendimentos <= 3000.00) {
-			System.out.printf("R$ %.2f", (rendimentos - 2000) * 0.08 );
+			System.out.printf("R$ %.2f\n", (rendimentos - 2000) * 0.08 );
 		}
 		else if(3000.01 <= rendimentos && rendimentos <= 4500.00) {
-			System.out.printf("R$ %.2f",  (3000 - 2000 )* 0.08 + (rendimentos - 3000) * 0.18);
+			System.out.printf("R$ %.2f\n",  (3000 - 2000 )* 0.08 + (rendimentos - 3000) * 0.18);
 		}
 		else if(rendimentos > 4500.00) {
-			System.out.printf("R$ %.2f", (3000 - 2000) * 0.08 + (4500 - 3000) * 0.18 +(rendimentos - 4500) * 0.28);
+			System.out.printf("R$ %.2f\n", (3000 - 2000) * 0.08 + 
+					(4500 - 3000) * 0.18 + (rendimentos - 4500) * 0.28);
 		}
 		
 		teclado.close();
