73ef4f7fa6cf3a1d2a9783d13e7ac68525f4d6ff
Curio5813 <marcosglasner@gmail.com>
2025-09-25 15:07:08 +0100

    Depurando programa 'oSalaoDoClube.java'.

--- src/BeecrowdAdHoc/oSalaoDoClube.java
+++ src/BeecrowdAdHoc/oSalaoDoClube.java
@@ -10,125 +10,70 @@ public class oSalaoDoClube {
 		
 		Scanner teclado = new Scanner(System.in);
 		
-		int m, n, area, largura, qtd_tabuas;
-		ArrayList<Integer> tabuas = new ArrayList<>();
 		
-		m = teclado.nextInt();
-		n = teclado.nextInt();
-		
-		while(m != 0 && n != 0) {
-			area = m * n * 100;
-			largura = teclado.nextInt();
-			qtd_tabuas = teclado.nextInt();
-			
-			if(area % largura != 0) {
-				System.out.println("impossivel");
-			}
-			else {
-				int numeroTabuas = 0, soma = 0, idx = 0;
-				boolean caso = false;
-				ArrayList<Integer> respostas = new ArrayList<>();
-				for(int i = 0; i < qtd_tabuas; i++) {
-					tabuas.add(teclado.nextInt());
-				}
-				
-				Collections.sort(tabuas);
-				ArrayList<Integer> invertida = new ArrayList<>(tabuas);
-				Collections.reverse(invertida);
-				for(int i = 0; i < invertida.size(); i++) {
-					for(int j = idx; j < tabuas.size(); j++) {
-						if(i == 0 && invertida.get(i) == m) {
-							numeroTabuas += 1;
-							soma += invertida.get(i);
-							if(soma * largura == area) {
-								respostas.add(numeroTabuas);
-								caso = true;
-								break;
-							}
-							break;
-						}
-						else if(invertida.get(i) == m) {
-							numeroTabuas += 1;
-							soma += invertida.get(i);
-							if(soma * largura == area) {
-								respostas.add(numeroTabuas);
-								caso = true;
-								break;
-							}
-							idx = j + 1;
-							break;
-						}
-						else if(invertida.get(i) + tabuas.get(j) == m) {
-							numeroTabuas += 2;
-							soma += invertida.get(i) + tabuas.get(j);
-							if(soma * largura == area) {
-								respostas.add(numeroTabuas);
-								caso = true;
-								break;
-							}
-							idx = j + 1;
-							break;
-						}
-					}
-					if(caso) {
-						break;
-					}
-				}
-				numeroTabuas = 0;
-				soma = 0;
-				idx = 0;
-				caso = false;
-				for(int i = 0; i < invertida.size(); i++) {
-					for(int j = idx; j < tabuas.size(); j++) {
-						if(i == 0 && invertida.get(i) == n) {
-							numeroTabuas += 1;
-							soma += invertida.get(i);
-							if(soma * largura == area) {
-								respostas.add(numeroTabuas);
-								caso = true;
-								break;
-							}
-							break;
-						}
-						else if(invertida.get(i) == n) {
-							numeroTabuas += 1;
-							soma += invertida.get(i);
-							if(soma * largura == area) {
-								respostas.add(numeroTabuas);
-								caso = true;
-								break;
-							}
-							idx = j + 1;
-							break;
-						}
-						else if(invertida.get(i) + tabuas.get(j) == n) {
-							numeroTabuas += 2;
-							soma += invertida.get(i) + tabuas.get(j);
-							if(soma * largura == area) {
-								respostas.add(numeroTabuas);
-								caso = true;
-								break;
-							}
-							idx = j + 1;
-							break;
-						}
-					}
-					if(caso) {
-						break;
-					}
-				}
-				if(!respostas.isEmpty()) {
-					System.out.println(Collections.min(respostas));
-				} else {
-					System.out.println("impossivel");
-				}
-			}
-			m = teclado.nextInt();
-			n = teclado.nextInt();
-			tabuas.clear();
-		}
-		
-		teclado.close();
+		while (true) {
+            int m = teclado.nextInt();
+            int n = teclado.nextInt();
+            if (m == 0 && n == 0) break;
+
+            int area = m * n * 100;
+            int largura = teclado.nextInt();
+            int qtd_tabuas = teclado.nextInt();
+            ArrayList<Integer> tabuas = new ArrayList<>();
+
+            for (int i = 0; i < qtd_tabuas; i++) {
+                tabuas.add(teclado.nextInt());
+            }
+
+            if (area % largura != 0) {
+                System.out.println("impossivel");
+                continue;
+            }
+
+            Collections.sort(tabuas);
+            ArrayList<Integer> invertida = new ArrayList<>(tabuas);
+            Collections.reverse(invertida);
+
+            Integer resultadoM = tentaPreencher(m, area, largura, tabuas, invertida);
+            Integer resultadoN = tentaPreencher(n, area, largura, tabuas, invertida);
+
+            if (resultadoM != null || resultadoN != null) {
+                ArrayList<Integer> respostas = new ArrayList<>();
+                if (resultadoM != null) respostas.add(resultadoM);
+                if (resultadoN != null) respostas.add(resultadoN);
+                System.out.println(Collections.min(respostas));
+            } else {
+                System.out.println("impossivel");
+            }
+        }
+
+        teclado.close();
+    }
+
+	    private static Integer tentaPreencher(int alvo, int area, int largura,
+	        ArrayList<Integer> tabuas, ArrayList<Integer> invertida)
+	    {
+	        int numeroTabuas = 0, soma = 0, idx = 0;
+	        for (int i = 0; i < invertida.size(); i++) {
+	            for (int j = idx; j < tabuas.size(); j++) {
+	                if (invertida.get(i) == alvo) {
+	                    numeroTabuas += 1;
+	                    soma += invertida.get(i);
+	                } else if (invertida.get(i) + tabuas.get(j) == alvo) {
+	                    numeroTabuas += 2;
+	                    soma += invertida.get(i) + tabuas.get(j);
+	                } else {
+	                    continue;
+	                }
+
+	                if (soma * largura == area) {
+	                    return numeroTabuas;
+	                }
+	                idx = j + 1;
+	                break;
+	            }
+	        }
+	        return null;
 	}
 
 }
