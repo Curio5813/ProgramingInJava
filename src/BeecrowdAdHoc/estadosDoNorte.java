362c4e7c94c8e5c312cf742419ab130dae122ae9
Curio5813 <marcosglasner@gmail.com>
2025-11-11 19:10:40 +0000

    Adicionando o programa 'estadosDoNorte.java' ao repositório.

---
+++ src/BeecrowdAdHoc/estadosDoNorte.java
@@ -0,0 +1,36 @@
+package BeecrowdAdHoc;
+
+
+import java.util.*;
+
+
+public class estadosDoNorte {
+	
+	public static void main(String[] args) {
+		
+		Scanner teclado = new Scanner(System.in);
+		
+		String[] estados = {"roraima", "acre", "amapa", "amazonas", "para", "rondonia", "tocantins"};
+		
+		boolean flag = false;
+		
+		String estado;
+		
+		estado = teclado.next();
+		
+		for(int i = 0; i < estados.length; i++) {
+			if(estado.contentEquals(estados[i])) {
+				System.out.println("Regiao Norte");
+				flag = true;
+				break;
+			}
+		}
+		if(!flag) {
+			System.out.println("Outra regiao");
+		}
+		
+		teclado.close();
+		
+	}
+
+}
