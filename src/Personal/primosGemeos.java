3ebf81366324ac54dbad86c6cfd28d48b22308f7
Curio5813 <marcosglasner@gmail.com>
2025-11-21 10:55:50 +0000

    Alterando o código para evitar acesso a index fora do intervalo.

--- src/Personal/primosGemeos.java
+++ src/Personal/primosGemeos.java
@@ -29,28 +29,36 @@ public class primosGemeos {
             for (int j = 0; j < primos.size(); j++) {
                 List<Integer> temp = new ArrayList<>();
                 if (i != j && primos.get(i) - primos.get(j) == 2) {
-                    temp.add(primos.get(i));
                     temp.add(primos.get(j));
+                    temp.add(primos.get(i));
                     gemeos.add(temp);
+                    break;
                 }
             }
         }
         for (int i = 0; i < gemeos.size(); i++) {
             for (int j = 0; j < primos.size(); j++) {
-                if (gemeos.get(i).getFirst() < primos.get(j)) {
-                    dist.add(primos.get(j) - gemeos.get(i).getFirst());
+                int maior = gemeos.get(i).get(1);
+                if (maior < primos.get(j)) {
+                    dist.add(primos.get(j) - gemeos.get(i).get(1));
                     break;
                 }
 
             }
         }
+        int tamDist = dist.size();
+        int tamGemeo = gemeos.size();
+        System.out.println(tamGemeo + " " + tamDist);
         System.out.println("Primo 1 - Primo 2 -> Distância ao próximo número primo:");
-        for (int i = 0; i < gemeos.size(); i++) {
-            System.out.println(gemeos.get(i).getLast() + " " + gemeos.get(i).getFirst() + " " + dist.get(i));
+        for (int i = 0; i < dist.size(); i++) {
+            int maior = gemeos.get(i).get(1);
+            int menor = gemeos.get(i).get(0);
+            System.out.println(menor + " " + maior + " " + dist.get(i));
         }
         int maiorValor = Collections.max(dist);
         int maior_idx = dist.indexOf(maiorValor);
+
         System.out.println("Maior distância encontrada:");
-        System.out.println(gemeos.get(maior_idx).get(1) + " " + gemeos.get(maior_idx).get(0) + " = " + dist.get(maior_idx));
+        System.out.println(gemeos.get(maior_idx).get(0) + " " + gemeos.get(maior_idx).get(1) + " " + (gemeos.get(maior_idx).get(1) +  dist.get(maior_idx)) + " = " + dist.get(maior_idx));
     }
 }
