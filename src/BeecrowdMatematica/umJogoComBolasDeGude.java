e2bcdb231ffc1599ff27d51130456d7a4314fbd8
Curio5813 <marcosglasner@gmail.com>
2025-11-24 23:42:06 +0000

    O programa 'umJogoComBolasDeGude.java' está debugado e em pleno funcionamento.

--- src/BeecrowdMatematica/umJogoComBolasDeGude.java
+++ src/BeecrowdMatematica/umJogoComBolasDeGude.java
@@ -17,56 +17,19 @@ public class umJogoComBolasDeGude {
             }
 
             List<Long> bacias = new ArrayList<>();   // ← agora é List<Long>
-            long rodadas = 0;
-            long i = 0;
+            long rodadas = 0, pDois = 1;
+            int i = 0, cont = 0;
 
             for (long j = 0; j < n; j++) {
                 bacias.add(teclado.nextLong());     // ← lê como long
             }
-
-            long soma = bacias.stream().mapToLong(Long::longValue).sum();
-
-            while (soma > 0) {
-                if (i == 0) {
-                    rodadas += bacias.get((int) i);
-                    bacias.set((int) i, 0L);
-                    i += 1;
-                }
-                else if (i > 0) {
-
-                    if (i >= n) {
-                        break;
-                    }
-
-                    if (bacias.get((int) i) == 0) {
-                        while (bacias.get((int) i) == 0) {
-                            i += 1;
-                            if (i >= n) {
-                                break;
-                            }
-                        }
-                    }
-
-                    if (i >= n) {
-                        break;
-                    }
-
-                    while (bacias.get((int) i) > 0 && i != 0) {
-                        bacias.set((int) i, bacias.get((int) i) - 1);
-                        rodadas += 1;
-
-                        for (long k = i - 1; k >= 0; k--) {
-                            bacias.set((int) k, bacias.get((int) k) + 1);
-                            i = k;
-                        }
-                    }
-                }
-
-                soma = bacias.stream().mapToLong(Long::longValue).sum();
+            while(cont < n){
+                rodadas += bacias.get(i) * pDois;
+                i += 1;
+                pDois *= 2;
+                cont += 1;
             }
-
             System.out.println(rodadas);
-
             n = teclado.nextLong();
         }
 
