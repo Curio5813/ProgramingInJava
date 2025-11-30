e69cd273a905f58014f959f7c469f311a4362eaf
Curio5813 <marcosglasner@gmail.com>
2025-11-30 00:20:29 +0000

    Adicionando o programa 'numerosPerfeitos.java' ao repositório.

---
+++ src/Personal/numerosPerfeitos.java
@@ -0,0 +1,21 @@
+package Personal;
+
+public class numerosPerfeitos {
+
+    public static void main(String[] args) {
+
+        for(int i = 1; i < 100000 + 1; i++){
+            int soma = 0;
+            for(int j = i - 1; j >= 1; j--){
+                if(i % j == 0){
+                    soma += j;
+                }
+            }
+            if(soma == i){
+                System.out.println(i + " é um numero perfeito.");
+            }
+        }
+
+    }
+
+}
