5f9d8adaad3a9761fc2d13c299249d3596f0414c
Curio5813 <marcosglasner@gmail.com>
2026-06-06 13:50:25 +0100

    Adicionado programas sobre estudos de series infinitas.

---
+++ src/Personal/RazaoNumericaNumerosIrracionaisERacionais.java
@@ -0,0 +1,16 @@
+package Personal;
+
+public class RazaoNumericaNumerosIrracionaisERacionais {
+
+    public static void main(String[] args) {
+
+        double p = Math.PI;
+
+        System.out.println(Math.pow((5.0/3.0) * p * Math.sqrt(2), 3));
+
+    }
+}
+
+/*
+Razão numerica entre numeros irracionais e numeros racionais.
+*/
---
+++ src/Personal/SerieDeBernoulliSomaDeFracoes.java
@@ -0,0 +1,19 @@
+package Personal;
+
+public class SerieDeBernoulliSomaDeFracoes {
+
+    public static void main(String[] args) {
+
+        double p = 1.0;
+        double limite = Math.pow(10, 11);
+        for(long i = 2; i <= limite; i++) {
+            p += 1/(double) i;
+        }
+        System.out.printf("%.10f%n", p);
+    }
+}
+
+/*
+100 bilhões ou 10¹¹ é igual a 25,9056516865, expoente primos vão digrecinar a convergência
+série. O Resultado deve ser igual há (5(√2π²)/3)³, converge para 406.0138611108578 .
+*/
