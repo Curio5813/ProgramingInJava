a235618f12dda726c3655a380c01d014d54374c2
Curio5813 <marcosglasner@gmail.com>
2026-06-10 12:51:29 +0100

    Alterando comentário.

--- src/Personal/SerieDeBernoulliSomaDeFracoes.java
+++ src/Personal/SerieDeBernoulliSomaDeFracoes.java
@@ -5,7 +5,7 @@ public class SerieDeBernoulliSomaDeFracoes {
     public static void main(String[] args) {
 
         double p = 1.0;
-        double limite = Math.pow(10, 11);
+        double limite = Math.pow(10, 6);
         for(long i = 2; i <= limite; i++) {
             p += 1/(double) i;
         }
@@ -16,5 +16,11 @@ public class SerieDeBernoulliSomaDeFracoes {
 /*
 100 bilhões ou 10¹¹ é igual a 25,9056516865, os numeros primos e seus expoente
 vão direcionar a convergência da série. O Resultado deve ser igual há (5(√2π²)/3)³,
-converge para 406.0138611108578 .
+converge para 406.0138611108578 . Assim:
+
+ [ ∫_(ℝ \ ℚ) x dμ(x) ] / [ ∫_(ℚ) x dμ(x) ] = (250 * √2π⁶) / 27
+
+ Representação:
+ Numerador (Irracionais): ∫_(ℝ \ ℚ) x dμ(x) = 250√2π⁶
+ Denominador (Racionais): ∫_(ℚ) x dμ(x) = 27
 */
